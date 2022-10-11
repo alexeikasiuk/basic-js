7840b20a6e488ddb87ec358ce03a9b5dabd67b2e
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-10-11 18:58:56 +0300

    init: start tast base-js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,37 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sample) {
+  // no arguments
+  if (arguments.length == 0) return false;
+
+  //not a string
+  if (typeof sample != 'string') return false;
+
+  let num = +sample;
+
+  //after parsing not a number or < 0
+  if (isNaN(num) || num <= 0) return false;
+
+  let res = Math.ceil(
+    Math.log(MODERN_ACTIVITY / num) / (0.693 / HALF_LIFE_PERIOD)
+  );
+
+  //inadequate values
+  return res < 0 ? false : res;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  if (!Array.isArray(matrix)) return false;
+  if (arguments.length == 0) return false;
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    let box = matrix[i];
+    for (let j = 0; j < box.length; j++) {
+      if (box[j] == '^^') count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
